c4334354c863fa09f2f5608d74a57c2023f8a41a
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-07 19:25:46 +0700

    commit 9

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -77,6 +77,29 @@ void binarySearch()
 
        int low = 0;
        int high = nPanjang -1;
-    } 
+       do
+       {
+        int mid = (low + high) /2;
+        if (element[mid]== x)
+        {
+            cout << "\n[/] Elemen " << x << "ditemukan pada index" << mid << ".\n";
+            return;
+        }
+        if (x < element [mid])
+        {
+            high = mid -1;
+        }
+        if (x > element[mid])
+        {
+            low = mid +1;
+        }
+       }while (low <=high);
+       if (low > high)
+       {
+        cout << "\n[x] elemen" << x << "tidak ditemukan dalam array .\n";
+       } 
+       cout << "\nIngin mencari lagi ? (y/n)";
+       cin >> ulang;
+    } while (ulang== 'y' || ulang == 'Y');
     
 }
