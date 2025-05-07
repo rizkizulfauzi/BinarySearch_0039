f6bb067e3e6eaad4b358510c0358d29bc0ee353c
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-07 19:03:38 +0700

    commit 4

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -20,4 +20,13 @@ void input()
             cout << " \n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi. \n";
         }
     }
+
+    cout <<"\n==================================\n";
+    cout <<"        Masukan Elemen Array \n";
+    cout <<"=============================\n";
+    for (int i = 0; i< nPanjang; i++)
+    {
+        cout << "Data ke-" <<(i + 1) <<"=";
+        cin >> element[i];
+    }
 }
