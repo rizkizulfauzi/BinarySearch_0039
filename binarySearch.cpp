6f852d5fe3ad3f21e1d9ce9fd4441067a715c237
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-07 19:12:20 +0700

    commit 6

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -48,3 +48,17 @@ void bubbleSortArray()
         pass++;
     }while (pass <= nPanjang -1);
 }
+
+void display()
+{
+    cout << "\n=================================================\n";
+    cout <<"       Element Array Setelah Diurutkan (Asc)   \n";
+    cout <<"===============================================\n";
+    for (int j=0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1){
+            cout<< "-->";
+        }
+    }
+}
