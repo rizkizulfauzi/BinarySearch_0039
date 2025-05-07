96e61c0c39b39a35b464ef77633558de3307b57d
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-07 18:56:50 +0700

    commit 2

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Memasukan banyak element pada array (maksimal 10) :";
+        cin >> nPanjang;
+    }
+}
